8618b6c87666aeb82d47db4cb299df3a0953b1ac
OracleTM <paurelijus@hotmail.com>
2025-05-21 22:42:16 +0300

    Add files via upload

--- enviroments.py
+++ enviroments.py
@@ -32,64 +32,64 @@ for key, value in os.environ.items():
 
 
 
-import tensorflow as tf
-print("Available GPUs:", tf.config.list_physical_devices('GPU'))
+# import tensorflow as tf
+# print("Available GPUs:", tf.config.list_physical_devices('GPU'))
 
 
 
-import tensorflow as tf
-import flask
-import mysql.connector
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from flask import Flask
+# import tensorflow as tf
+# import flask
+# import mysql.connector
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# from flask import Flask
 
-#  1. Check TensorFlow Setup
-print("\n TensorFlow Version:", tf.__version__)
-print(" TensorFlow Device List:", tf.config.list_physical_devices('GPU' if tf.config.list_physical_devices('GPU') else 'CPU'))
+# #  1. Check TensorFlow Setup
+# print("\n TensorFlow Version:", tf.__version__)
+# print(" TensorFlow Device List:", tf.config.list_physical_devices('GPU' if tf.config.list_physical_devices('GPU') else 'CPU'))
 
-# Simple TensorFlow operation
-x = tf.constant([[2.0, 3.0]])
-w = tf.Variable([[4.0], [5.0]])
-y = tf.matmul(x, w)
-print("\n TensorFlow Matrix Multiplication Result:\n", y.numpy())
+# # Simple TensorFlow operation
+# x = tf.constant([[2.0, 3.0]])
+# w = tf.Variable([[4.0], [5.0]])
+# y = tf.matmul(x, w)
+# print("\n TensorFlow Matrix Multiplication Result:\n", y.numpy())
 
-#  2. Check MySQL Connection
-try:
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="ORACLETM",      # Change to your MySQL username
-        password="akle7",  # Change to your MySQL password
-        database="test_db"  # Make sure the database exists
-    )
-    print("\n MySQL Connection Successful!")
-    conn.close()
-except mysql.connector.Error as err:
-    print("\n MySQL Connection Failed!", err)
+# #  2. Check MySQL Connection
+# try:
+#     conn = mysql.connector.connect(
+#         host="localhost",
+#         user="ORACLETM",      # Change to your MySQL username
+#         password="akle7",  # Change to your MySQL password
+#         database="test_db"  # Make sure the database exists
+#     )
+#     print("\n MySQL Connection Successful!")
+#     conn.close()
+# except mysql.connector.Error as err:
+#     print("\n MySQL Connection Failed!", err)
 
-#  3. Check Flask Server
-app = Flask(__name__)
+# #  3. Check Flask Server
+# app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return " Flask Server is Running!"
+# @app.route('/')
+# def home():
+#     return " Flask Server is Running!"
 
-if __name__ == '__main__':
-    print("\n Starting Flask Server... Open 'http://127.0.0.1:5000' in your browser.")
-    app.run(debug=True, use_reloader=False)
+# if __name__ == '__main__':
+#     print("\n Starting Flask Server... Open 'http://127.0.0.1:5000' in your browser.")
+#     app.run(debug=True, use_reloader=False)
 
-#  4. Generate a Matplotlib Pop-Up Plot
-sns.set_theme(style="darkgrid")
-data = np.random.rand(100, 2)
-df = pd.DataFrame(data, columns=["X", "Y"])
-plt.figure(figsize=(6, 4))
-sns.scatterplot(x="X", y="Y", data=df)
-plt.title(" Matplotlib & Seaborn Test Plot")
-plt.show()
+# #  4. Generate a Matplotlib Pop-Up Plot
+# sns.set_theme(style="darkgrid")
+# data = np.random.rand(100, 2)
+# df = pd.DataFrame(data, columns=["X", "Y"])
+# plt.figure(figsize=(6, 4))
+# sns.scatterplot(x="X", y="Y", data=df)
+# plt.title(" Matplotlib & Seaborn Test Plot")
+# plt.show()
 
 
-# - Integrating TensorFlow models into Flask
-# - Building an interactive data dashboard with Dash. 
-# - Structuring a Flask-based full-stack application
+# # - Integrating TensorFlow models into Flask
+# # - Building an interactive data dashboard with Dash. 
+# # - Structuring a Flask-based full-stack application
